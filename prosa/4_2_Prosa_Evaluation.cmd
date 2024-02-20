26c1d15d53f492caaad121f138a0a61012412f8d
FrankPolak <160171754+FrankPolak@users.noreply.github.com>
2024-02-20 21:55:29 +0100

    Create 4_2_Prosa_Evaluation.cmd

---
+++ prosa/4_2_Prosa_Evaluation.cmd
@@ -0,0 +1,13 @@
+read pdb 1meeA.pdb 1mee
+read pdb 1scj_A.pdb 1scj
+read pdb 1sbh_A.pdb 1sbh
+read pdb P11018.BL0000XX.pdb mdlXX
+read pdb P11018.BL0000YY.pdb mdlYY
+
+analyse energy *
+winsize * 40
+
+color * mdlXX red
+color * mdlYY cyan
+
+plot
